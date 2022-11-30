111cdbeba701655e038e6dcf812d94eb362b8cda
Iosefa Percival <ipercival@gmail.com>
2022-11-30 16:00:08 +0900

    [scripts] moves python scripts to a scripts directory that calls module class functions
    Signed-off-by: Iosefa Percival <ipercival@gmail.com>

---
+++ scripts/initialize_tables.py
@@ -0,0 +1,11 @@
+import argparse
+
+from src.initializer import InitializeMapillary
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Creates mly_images table if it doesnt exist already.'
+    )
+    initializer = InitializeMapillary()
+    initializer.create_image_table()
